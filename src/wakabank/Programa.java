fd65510da2081bdec1e01344ec79822496434f23
Letícia <leticia-dospassos@hotmail.com>
2021-07-10 12:49:35 -0300

    WakaBank

--- src/wakabank/Programa.java
+++ src/wakabank/Programa.java
@@ -5,6 +5,7 @@ public class Programa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Tivemos algumas problemas, mas estamos de volta. Pretas e pretos no topo!");
+        System.out.println("\nMelhor Banco desde já");
 	}
 
 }
