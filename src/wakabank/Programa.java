7e79abf774d7fe27d7d694e873798d735f4d4e49
Carlos Henrique <carlosh.carmo@hotmail.com>
2021-07-10 12:28:22 -0300

    Mensagem de boas vindas

---
+++ src/wakabank/Programa.java
@@ -0,0 +1,10 @@
+package wakabank;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Tivemos algumas problemas, mas estamos de volta. Pretas e pretos no topo!");
+	}
+
+}
