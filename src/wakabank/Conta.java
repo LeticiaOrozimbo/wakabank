3a1f6fb8a9edcee27cc0d247b06461df900ce629
joaovitorbrandao7 <joaovitor.brandao21@outlook.com>
2021-07-11 10:19:01 -0300

    Criada a classe conta

---
+++ src/wakabank/Conta.java
@@ -0,0 +1,102 @@
+package wakabank;
+
+public class Conta {
+	
+	private int numConta;
+	private int numAgencia;
+	private String nomeTitular;
+	private String numRG;
+	private String numCPF;
+	private String endereco;
+	private int numTelefone;
+	private String email;
+	private double saldo;
+	
+	public Conta(int numConta, int numAgencia, String nomeTitular, String numRG, String numCPF, String endereco, int numTelefone, String email, double saldo) {
+		super();
+		this.numConta = numConta;
+		this.numAgencia = numAgencia;
+		this.nomeTitular = nomeTitular;
+		this.numRG = numRG;
+		this.numCPF = numCPF;
+		this.endereco = endereco;
+		this.numTelefone = numTelefone;
+		this.email = email;
+		this.saldo = saldo;
+		
+	}
+
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+
+	public int getNumAgencia() {
+		return numAgencia;
+	}
+
+	public void setNumAgencia(int numAgencia) {
+		this.numAgencia = numAgencia;
+	}
+
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+
+	public String getNumRG() {
+		return numRG;
+	}
+
+	public void setNumRG(String numRG) {
+		this.numRG = numRG;
+	}
+
+	public String getNumCPF() {
+		return numCPF;
+	}
+
+	public void setNumCPF(String numCPF) {
+		this.numCPF = numCPF;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getNumTelefone() {
+		return numTelefone;
+	}
+
+	public void setNumTelefone(int numTelefone) {
+		this.numTelefone = numTelefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+}
